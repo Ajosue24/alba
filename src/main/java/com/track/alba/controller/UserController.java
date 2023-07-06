34698c629246ddbfb48c4163b142aff111a9a07f
Ajosue25 <alexis.vasquez@mercadolibre.com.co>
2023-07-05 19:55:03 -0500

    delete comment

--- src/main/java/com/track/alba/controller/UserController.java
+++ src/main/java/com/track/alba/controller/UserController.java
@@ -12,7 +12,6 @@ public class UserController {
         user.setId(1L);
         user.setName("John Doe");
         user.setEmail( "some@gmail.com");
-        //Hola luis
         return user;
     }
 
