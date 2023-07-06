18e30a232ca4c4dc5d44cef9601476ee9931aad6
Ajosue25 <alexis.vasquez@mercadolibre.com.co>
2023-07-05 19:42:54 -0500

    firts commit

---
+++ src/main/java/com/track/alba/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.track.alba.controller;
+
+import com.track.alba.entity.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping
+    User getUser() {
+        var user = new User();
+        user.setId(1L);
+        user.setName("John Doe");
+        user.setEmail( "some@gmail.com");
+        return user;
+    }
+
+    @PostMapping
+    void createUser(@RequestBody User user) {
+        System.out.println(user);
+
+    }
+}
---
+++ src/main/java/com/track/alba/entity/User.java
@@ -0,0 +1,11 @@
+package com.track.alba.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    long id;
+    String name;
+    String email;
+    String address;
+}
